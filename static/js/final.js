20eb97dff48f9c51f717e66716ce59762bd759ec
Vasundhara Rakesh <vasu9498@gmail.com>
2018-01-09 00:08:46 +0530

    Add files via upload

---
+++ static/js/final.js
@@ -0,0 +1,19 @@
+$(function(){
+	$(document).on('keydown', function(e){
+        if ( e.which == 68 ){
+    		$.ajax({
+	    		url: '/logData',
+		    	data: { json : JSON.stringify(data)} ,
+			    type: 'POST',
+                dataType : 'json', 
+    			success: function(response){
+	    			console.log(response);
+                    window.location.href="/startTrial";
+			    },
+    			err: function(error){
+	    			console.log("error");
+		        }
+    		});
+        }
+	});
+});
---
+++ static/js/finish.js
@@ -0,0 +1,7 @@
+$(function(){
+	$(document).on('keydown', function(e){
+        if ( e.which == 68 ){
+            window.location.href="/finish"
+        }
+	})
+});
